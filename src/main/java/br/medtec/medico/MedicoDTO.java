4530cc1b4a03cf0059344beb35a9a85b19689f21
Richard Fernandes <richard.fernandes@mosistemas.com>
2024-07-30 17:02:25 -0300

    feature(medicoDto): criado dto para receber o json do medico.

--- src/main/java/br/medtec/medico/MedicoDTO.java
+++ src/main/java/br/medtec/medico/MedicoDTO.java
@@ -1,4 +1,36 @@
 package br.medtec.medico;
 
+import br.medtec.entity.Paciente;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class MedicoDTO {
+    private String oid;
+    private String nome;
+    private String emailContato;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private String crm;
+
+    public Medico toEntity() {
+        Medico medico = new Medico();
+        toEntity(medico);
+        return medico;
+    }
+
+    public Medico toEntity(Medico medico) {
+        medico.setNome(this.nome);
+        medico.setEmailContato(this.emailContato);
+        medico.setSobrenome(this.sobrenome);
+        medico.setTelefone(this.telefone);
+        medico.setCpf(this.cpf);
+        medico.setCrm(this.crm);
+        return medico;
+    }
 }
