930787c08960e106f9a74cdd24c4168a8c1435b7
Richard Fernandes <richard.fernandes@mosistemas.com>
2024-07-31 14:14:17 -0300

    feat(repository): alterado estrutura do repository

--- src/main/java/br/medtec/interfaces/GenericRepository.java
+++ src/main/java/br/medtec/interfaces/GenericRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GenericRepository<T> {
-    Optional<T> findByOid(String oid);
+    T findByOid(String oid);
     List<T> findAll();
     void save(T entity);
     T update(T entity);
     void deleteByOid(String oid);
     void delete(T entity);
     List<T> findByAttribute(String atributeName, Object value);
+    boolean existsByAttribute(String atributeName, Object value);
 }
